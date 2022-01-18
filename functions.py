180dbc79fdf8ef40ab7d8e9cca69919bddc64361
Fractalego <albertocetoli@gmail.com>
2022-01-18 18:51:59 +0000

    Rules for deleting items from shopping list

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import logging
 import json
 import requests
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from fuzzywuzzy import process
 from wafl.exceptions import CloseConversation, InterruptTask
 
@@ -34,6 +34,19 @@ def add_shopping_list(item):
     json.dump(shopping_list, open("shopping_list.json", "w"))
 
 
+def remove_from_shopping_list(item):
+    extracted, score = process.extract(item, shopping_list, limit=1)[0]
+    if score < 60:
+        {f"% SAY I did not quite get the item to remove %"}
+        return
+
+    if not {f"% Do you want to remove {extracted} from the shopping list? %"}:
+        return False
+    
+    shopping_list.remove(extracted)
+    json.dump(shopping_list, open("shopping_list.json", "w"))
+
+    
 def reset_shopping_list():
     global shopping_list
     shopping_list = []
@@ -43,6 +56,10 @@ def reset_shopping_list():
 def get_time():
     now = datetime.now()
     return now.strftime("%H,%M")
+
+def get_date():
+    now = datetime.now()
+    return now.strftime("%A %d %B %Y")
 
 
 def close_conversation():
@@ -55,9 +72,10 @@ def close_task():
 
 def normalize_name(linename):
     extracted, score = process.extract(linename, lines_dict.keys(), limit=1)[0]
-    if score < 70:
-        {f"% I did not quite get the line name %"}
-        return False
+    print("EXTRACTED!", extracted, score)
+    if score < 60:
+        {f"% SAY I did not quite get the line name %"}
+        return
 
     return extracted
 
@@ -78,4 +96,52 @@ def check_tfl_line(linename):
             return
 
     {f"% SAY The {linename} line is running normally %"}
+
+
+def check_today_weather():
+    latitude = "51.5390"
+    longitude = "-0.1426"
+    secrets = json.load(open("secrets.json"))
+
+    result = requests.get(f"https://rgw.5878-e94b1c46.eu-gb.apiconnect.appdomain.cloud/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=true&includeLocationName=true&latitude={latitude}&longitude={longitude}",
+                      headers={"X-IBM-Client-Id": secrets['key'],
+                               "X-IBM-Client-Secret": secrets['secret']})
+    data = result.json()
+    if 'features' not in data:
+        {"% SAY There is a connection error to the weather API. Please try later. %"}
+        return
+    
+    today = datetime.now().strftime("%Y-%m-%d")
+    
+    for item in data['features'][0]['properties']['timeSeries']:
+        if today in item['time']: 
+            {f"% SAY Today the temperature should be between {int(item['dayLowerBoundMaxTemp'])} and {int(item['dayUpperBoundMaxTemp'])}. %"}
+            if item['dayProbabilityOfPrecipitation'] != 0:
+                {f"% SAY The probability of rain is {item['dayProbabilityOfPrecipitation']} percent. %"}
+
+            if item['dayProbabilityOfSnow'] != 0:
+                {f"% SAY The probability of snow is {item['dayProbabilityOfSnow']} percent. %"}
+
+def check_tomorrow_weather():
+    latitude = "51.5390"
+    longitude = "-0.1426"
+    secrets = json.load(open("secrets.json"))
+
+    result = requests.get(f"https://rgw.5878-e94b1c46.eu-gb.apiconnect.appdomain.cloud/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=true&includeLocationName=true&latitude={latitude}&longitude={longitude}",
+                      headers={"X-IBM-Client-Id": secrets['key'],
+                               "X-IBM-Client-Secret": secrets['secret']})
+    data = result.json()
+    if 'features' not in data:
+        {"% SAY There is a connection error to the weather API. Please try later. %"}
+        return
+
+    tomorrow = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+    for item in data['features'][0]['properties']['timeSeries']:
+        if tomorrow in item['time']:
+            {f"% SAY Tomorrow the temperature should be between {int(item['dayLowerBoundMaxTemp'])} and {int(item['dayUpperBoundMaxTemp'])}. %"}
+            if item['dayProbabilityOfPrecipitation'] != 0:
+                {f"% SAY The probability of rain is {item['dayProbabilityOfPrecipitation']} percent. %"}
+
+            if item['dayProbabilityOfSnow'] != 0:
+                {f"% SAY The probability of snow is {item['dayProbabilityOfSnow']} percent. %"}
 
