92d2d384ff252f541693dc6f9dbec45fd3774a77
Fractalego <albertocetoli@gmail.com>
2022-10-29 10:41:05 +0100

    calling local functions within functions.py

--- functions.py
+++ functions.py
@@ -64,7 +64,7 @@ def reset_shopping_list():
 
 def get_time():
     now = datetime.now()
-    return now.strftime("%H; %M")
+    return now.strftime("%H %M")
 
 
 def get_date():
@@ -109,47 +109,25 @@ def check_tfl_line(linename):
 
     {f"% SAY The {linename} line is running normally %"}
 
+    
 
 def check_today_weather():
     latitude = "51.5390"
     longitude = "-0.1426"
-    secrets = json.load(open("secrets.json"))
-
-    result = requests.get(
-        f"https://rgw.5878-e94b1c46.eu-gb.apiconnect.appdomain.cloud/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=true&includeLocationName=true&latitude={latitude}&longitude={longitude}",
-        headers={
-            "X-IBM-Client-Id": secrets["key"],
-            "X-IBM-Client-Secret": secrets["secret"],
-        },
-    )
-    data = result.json()
-    if "features" not in data:
-        {"% SAY There is a connection error to the weather API. Please try later. %"}
-        return
-
     today = datetime.now().strftime("%Y-%m-%d")
+    check_weather_lat_long(latitude, longitude, today)
 
-    for item in data["features"][0]["properties"]["timeSeries"]:
-        if today in item["time"]:
-            {
-                f"% SAY Today the temperature should be between {int(item['dayLowerBoundMaxTemp'])} and {int(item['dayUpperBoundMaxTemp'])}. %"
-            }
-            if item["dayProbabilityOfPrecipitation"] != 0:
-                {
-                    f"% SAY The probability of rain is {item['dayProbabilityOfPrecipitation']} percent. %"
-                }
-
-            if item["dayProbabilityOfSnow"] != 0:
-                {
-                    f"% SAY The probability of snow is {item['dayProbabilityOfSnow']} percent. %"
-                }
-
-
+    
 def check_tomorrow_weather():
     latitude = "51.5390"
     longitude = "-0.1426"
-    secrets = json.load(open("secrets.json"))
+    today = datetime.now()
+    tomorrow = (today + timedelta(days=1)).strftime("%Y-%m-%d")
+    check_weather_lat_long(latitude, longitude, tomorrow)
 
+
+def check_weather_lat_long(latitude, longitude, day):
+    secrets = json.load(open("secrets.json"))
     result = requests.get(
         f"https://rgw.5878-e94b1c46.eu-gb.apiconnect.appdomain.cloud/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=true&includeLocationName=true&latitude={latitude}&longitude={longitude}",
         headers={
@@ -162,18 +140,12 @@ def check_tomorrow_weather():
         {"% SAY There is a connection error to the weather API. Please try later. %"}
         return
 
-    tomorrow = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+
     for item in data["features"][0]["properties"]["timeSeries"]:
-        if tomorrow in item["time"]:
-            {
-                f"% SAY Tomorrow the temperature should be between {int(item['dayLowerBoundMaxTemp'])} and {int(item['dayUpperBoundMaxTemp'])}. %"
-            }
+        if day in item["time"]:
+            {f"% SAY The temperature should be between {int(item['dayLowerBoundMaxTemp'])} and {int(item['dayUpperBoundMaxTemp'])}. %"}
             if item["dayProbabilityOfPrecipitation"] != 0:
-                {
-                    f"% SAY The probability of rain is {item['dayProbabilityOfPrecipitation']} percent. %"
-                }
+                {f"% SAY The probability of rain is {item['dayProbabilityOfPrecipitation']} percent. %"}
 
             if item["dayProbabilityOfSnow"] != 0:
-                {
-                    f"% SAY The probability of snow is {item['dayProbabilityOfSnow']} percent. %"
-                }
+                {f"% SAY The probability of snow is {item['dayProbabilityOfSnow']} percent. %"}
